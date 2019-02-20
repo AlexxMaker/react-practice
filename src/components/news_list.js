878ec8bddf54a9643621bd3f05f1793dce0bf09b
AlexxMaker <Alexeus92@mail.ru>
2019-02-20 23:16:56 +0430

    Props Children F

--- src/components/news_list.js
+++ src/components/news_list.js
@@ -1,21 +1,20 @@
 import React from 'react';
+import NewsItem from './news_list_item'
 
 const NewsList = (props) => {
 
     const items = props.news.map((item)=>{
         return(
-            <div>
-                <h3>{item.title}</h3>
-                <div>
-                    {item.feed}
-                </div>
-            </div>
+            <NewsItem key={item.id} item={item}/>
         )
     });
 
     return (
-        <div>{items}</div>
         
+        <div>
+            {props.children}
+            {items}
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ class App extends Component {
         return (
             <div>
                 <Header />
-                <NewsList news={this.state.news}/> 
+                <NewsList news={this.state.news}>
+                    <h3>News are:</h3>
+                </NewsList> 
             </div>
         );
     }
