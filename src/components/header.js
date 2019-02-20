2fd5701a2273b8047527fa744e8974a4b7d85991
AlexxMaker <Alexeus92@mail.ru>
2019-02-20 21:00:38 +0430

    Props 1 S

--- src/components/header.js
+++ src/components/header.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 
 
 
+
 class Header extends Component {
+
+    state = {
+        keywords: 'Hello',
+    }
+
     inputChangeHandler (event) {
-        console.log(event.target.value);
+        this.setState({
+            keywords: event.target.value
+        })
     }
 
     render() {
+        console.log(this.state.keywords)
         return (
             <header>
                 <div className="logo">Logo</div>
-                <input type="text" onChange={this.inputChangeHandler}/>
+                <input type="text" onChange={this.inputChangeHandler.bind(this)}/>
             </header>
             )
         }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import json from './db.json'
+
 //COMPONENTS
 import Header from './components/header'
 
 
 const App = () => {
+    console.log(json)
     return (
         <Header />
     )
